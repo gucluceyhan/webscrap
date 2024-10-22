e8ebd496764a6c568f0f73cf6a576116aacd9719
Guclu Ceyhan <gucluceyhan@gmail.com>
2024-10-22 14:11:07 +0300

    En son hali
    arama motoru değiştirildi, gecikme kaldırıldı.

--- gecikmeli webscrapper.py
+++ gecikmeli webscrapper.py
@@ -2,9 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 import random
+import os
 
 # Excel dosyasından depo isimlerini çekme
 excel_file_path = '/Users/gucluceyhan/Documents/EczaDepoları/Ecza Depoları İsimleri.xlsx'
@@ -16,33 +20,31 @@ depo_list = depo_df['Depo'].tolist()
 # Selenium için driver kurulum
 chrome_options = Options()
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-service = Service('/usr/local/bin/chromedriver')
+chromedriver_path = os.getenv('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
+service = Service(chromedriver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Web adresleri için boş liste
 websites = []
 
-# Her bir depo için Google'da arama yap
+# Her bir depo için Bing'de arama yap
 for depo in depo_list:
-    driver.get("https://www.google.com")
-    search_box = driver.find_element("name", "q")
+    driver.get("https://www.bing.com")
+    search_box = driver.find_element(By.NAME, "q")
     search_box.send_keys(f"{depo} web adresi")
     search_box.send_keys(Keys.RETURN)
     
-    # Rastgele gecikme ekleme
-    time.sleep(random.uniform(3, 6))  # 3-6 saniye arasında rastgele bir bekleme
-
-    # İlk sonucu almak için deneme
+    # Sayfanın yüklenmesini beklemek için WebDriverWait kullanma
     try:
-        # Eğer CAPTCHA çıkarsa manuel müdahale için durdurma
-        input("Eğer CAPTCHA ile karşılaşırsanız, manuel olarak geçin ve devam etmek için ENTER tuşuna basın...")
-
-        # İlk sonuçtaki URL'yi alma
-        first_result = driver.find_element("css selector", 'div.yuRUbf > a')
+        first_result = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'li.b_algo h2 a'))
+        )
         website = first_result.get_attribute('href')
         websites.append(website)
     except Exception as e:
         websites.append('Not Found')
+        print("CAPTCHA tespit edildi veya sayfa yüklenemedi. CAPTCHA'ı manuel olarak geçin ve devam etmek için ENTER tuşuna basın.")
+        input("Devam etmek için ENTER tuşuna basın...")  # Kullanıcıdan ENTER tuşuna basmasını bekler
 
 # Sonuçları DataFrame'e aktar
 depo_df['Web Adresi'] = websites
